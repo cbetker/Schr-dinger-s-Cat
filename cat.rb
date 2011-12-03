85b916f07251553d2fde76c1c5cffd4d4cc8a6c6
Chris Betker <c_betker@hotmail.com>
2011-12-02 20:49:16 -0600

    code changed to reflect actual experiment

--- cat.rb
+++ cat.rb
@@ -1,23 +1,10 @@
-def time_diff_milli(start, finish)
-  (finish - start) * 1000.0
-end
 Shoes.app do
-  t1 = Time.now
-  
   button("Open Box")do
-    t2 = Time.now
-    time = time_diff_milli(t1, t2)
-    time = (time / 100).to_i
-    death_counter = []
-    time.times { death_counter << rand(1000)}
-    alive = true
-    death_counter.each do | test |
-      if test == 666
-        alive = false
-      end
+    if (rand(2) == 0)
+      puts "Your cat is alive!  Yay!"
+    else
+      puts "Your poor cat is dead..."
     end
-    alert("Your poor cat is dead")unless(alive)
-    alert("Your cat is alive!  Yay!")if(alive)
   end
   button("Exit")do
     exit!
